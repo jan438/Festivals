4cf9f9419e1dc4665391fa8f3f7467d6d4a04fec
jan438 <jan438@users.noreply.github.com>
2026-05-24 15:40:48 +0200

    Update import.py

--- import.py
+++ import.py
@@ -3,6 +3,10 @@ from reportlab.lib.pagesizes import A3, A4
 
 m = __import__('math', globals(), locals(), fromlist=['factorial'])
 
+maditsi_car = 111
+
+print(f'{maditsi_car=}')
+
 print(m.factorial(5), time.time())
 
 key = input("Wait")
