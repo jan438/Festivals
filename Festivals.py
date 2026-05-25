f3705965a2fbff7034242bca5eee9bea323e9c27
jan438 <jan438@users.noreply.github.com>
2026-05-25 13:54:14 +0200

    infobox added

--- Festivals.py
+++ Festivals.py
@@ -6,10 +6,23 @@ import sys
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+from reportlab.graphics import renderPDF
 
 festivalfont = "LiberationSerif"
 templatedata = []
 
+def scaleSVG(svgfile, scaling_factor):
+    svg_root = load_svg_file(svgfile)
+    svgRenderer = SvgRenderer(svgfile)
+    drawing = svgRenderer.render(svg_root)
+    scaling_x = scaling_factor
+    scaling_y = scaling_factor
+    drawing.width = drawing.minWidth() * scaling_x
+    drawing.height = drawing.height * scaling_y
+    drawing.scale(scaling_x, scaling_y)
+    return drawing
+    
 def create_Fesival_pdf(filename, ps, pagesize, title="Festivals"):
     try:
         c = canvas.Canvas(filename, pagesize=pagesize)
@@ -21,6 +34,8 @@ def create_Fesival_pdf(filename, ps, pagesize, title="Festivals"):
         c.drawCentredString(width / 2, height - titley_value, title)
         c.setLineWidth(1)
         c.rect(20, 20, width - 40, height - 40)
+        drawing = scaleSVG('SVG/infobox.svg', 0.1)
+        renderPDF.draw(drawing, c, 50, 775)
         c.showPage()
         c.save()
         print(f"✅ PDF Festivals '{filename}' created successfully.")
