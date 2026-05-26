7de307399aa8defe7d4117646b3f8c3abb6c6219
jan438 <jan438@users.noreply.github.com>
2026-05-26 15:10:00 +0200

    penciltip added

--- Festivals.py
+++ Festivals.py
@@ -9,6 +9,8 @@ from reportlab.pdfbase.pdfmetrics import registerFontFamily
 from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 from reportlab.graphics import renderPDF
 from reportlab.lib.colors import HexColor
+from reportlab.lib.colors import tan, black, green
+from reportlab.lib.units import inch
 
 festivalfont = "LiberationSerif"
 templatedata = []
@@ -70,6 +72,33 @@ def cadre(c, pagesize):
         c.line(i * dx, 0, i * dx, height)
     for i in range(15):
         c.line(0, i * dx, width, i * dx)
+
+def penciltip(canvas, debug=1):
+    u = inch/10.0
+    canvas.setLineWidth(4)
+    if debug:
+        canvas.scale(2.8,2.8) # make it big
+        canvas.setLineWidth(1) # small lines
+    canvas.setStrokeColor(black)
+    canvas.setFillColor(tan)
+    p = canvas.beginPath()
+    p.moveTo(10*u,0)
+    p.lineTo(0,5*u)
+    p.lineTo(10*u,10*u)
+    p.curveTo(11.5*u,10*u, 11.5*u,7.5*u, 10*u,7.5*u)
+    p.curveTo(12*u,7.5*u, 11*u,2.5*u, 9.7*u,2.5*u)
+    p.curveTo(10.5*u,2.5*u, 11*u,0, 10*u,0)
+    canvas.drawPath(p, stroke=1, fill=1)
+    canvas.setFillColor(black)
+    p = canvas.beginPath()
+    p.moveTo(0,5*u)
+    p.lineTo(4*u,3*u)
+    p.lineTo(5*u,4.5*u)
+    p.lineTo(3*u,6.5*u)
+    canvas.drawPath(p, stroke=1, fill=1)
+    if debug:
+        canvas.setStrokeColor(green) # put in a frame of reference
+        canvas.grid([0,5*u,10*u,15*u], [0,5*u,10*u])
         
 def create_Fesival_pdf(filename, ps, pagesize, title="Festivals"):
     try:
@@ -93,6 +122,7 @@ def create_Fesival_pdf(filename, ps, pagesize, title="Festivals"):
         drawrrrrRect(c,  30,  dy, 100, 200, 50, "#80ff84")
         drawrrhhRect(c, 230,  dy, 100, 200, 50, "#80ff84")
         drawhhrrRect(c, 430,  dy, 100, 200, 50, "#80ff84")
+        penciltip(c, False)
         c.showPage()
         c.save()
         print(f"✅ PDF Festivals '{filename}' created successfully.")
