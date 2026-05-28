591f22ebcd0c9a8aefffc5210398afe2c364c873
jan438 <jan438@users.noreply.github.com>
2026-05-28 14:21:19 +0200

    top added

--- Festivals.py
+++ Festivals.py
@@ -84,7 +84,7 @@ def drawbottomroundRect(c, x, y, w, h, a, color):
     p.lineTo(x + w, y)
     p.arcTo(x + w, y, x + w + a, y + a, startAng = 270, extent = 90)
     p.lineTo(x + w + a, y + h + 0.5 * a)
-    p.lineTo(x + 0.5 * a, y + h + 0.5 * a)
+    p.lineTo(x, y + h + 0.5 * a)
     p.lineTo(x, y + 0.5 * a)
     c.drawPath(p, stroke = 0, fill = 1)
     
