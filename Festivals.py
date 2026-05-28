b101ce79a527188e9cb818f3470df099272d86ab
jan438 <jan438@users.noreply.github.com>
2026-05-28 13:55:47 +0200

    top added

--- Festivals.py
+++ Festivals.py
@@ -64,6 +64,18 @@ def drawleftroundRect(c, x, y, w, h, a, color):
     p.lineTo(x, y + 0.5 * a)
     c.drawPath(p, stroke = 0, fill = 1)
     
+def drawtoproundRect(c, x, y, w, h, a, color):    
+    c.setFillColor(HexColor(color))
+    p = c.beginPath()
+    p.moveTo(x, y + 0.5 * a)
+    p.lineTo(x + w, y)
+    p.lineTo(x + w + a, y + h)
+    p.arcTo(x + w, y + h, x + w + a, y + h + a, startAng = 0, extent = 90)
+    p.lineTo(x + 0.5 * a, y + h + a)
+    p.arcTo(x, y + h, x + a, y + h + a, startAng = 90, extent = 90)
+    p.lineTo(x, y + 0.5 * a)
+    c.drawPath(p, stroke = 0, fill = 1)    
+    
 def cadre(c, pagesize):
     width = pagesize[0]
     height = pagesize[1]
@@ -121,7 +133,8 @@ def create_Fesival_pdf(filename, ps, pagesize, title="Festivals"):
         dy = width / 10
         drawallroundRect(c,  30,  dy, 1, 1, 50, "#80ff84")
         drawrightroundRect(c,  230,  dy, 1, 40, 50, "#80ff84")
-        drawleftroundRect(c,  430,  dy, 40, 40, 50, "#80ff84")
+        drawleftroundRect(c,  430,  dy, 1, 40, 50, "#80ff84")
+        drawtoproundRect(c,  230,  3 * dy, 40, 40, 50, "#80ff84")
         penciltip(c, 10, 50, True)
         c.showPage()
         c.save()
