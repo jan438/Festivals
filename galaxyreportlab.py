05059a0286f5048f20d0ff5e8734d818edaa7f5f
jan438 <jan438@users.noreply.github.com>
2026-06-04 09:27:52 +0200

    galaxyreportlab added

---
+++ galaxyreportlab.py
@@ -0,0 +1,35 @@
+from reportlab.lib.pagesizes import A3, A4
+from reportlab.pdfgen import canvas
+import os
+import csv
+import sys
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
+from reportlab.graphics import renderPDF
+from reportlab.lib.colors import yellow, green, red, black, HexColor
+from reportlab.lib.colors import tan, black, green
+from reportlab.lib.units import inch, cm, mm
+from math import pi, cos, sin, radians, sqrt
+
+galaxyfont = "LiberationSerif"
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Festivals'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Festivals"
+os.chdir(path)
+
+pdfmetrics.registerFont(TTFont('LiberationSerif', 'LiberationSerif-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('LiberationSerifBold', 'LiberationSerif-Bold.ttf'))
+pdfmetrics.registerFont(TTFont('LiberationSerifItalic', 'LiberationSerif-Italic.ttf'))
+pdfmetrics.registerFont(TTFont('LiberationSerifBoldItalic', 'LiberationSerif-BoldItalic.ttf'))
+
+c = canvas.Canvas("PDF/Galaxy.pdf", pagesize=A3)
+c.setTitle("Galaxy")
+width, height = A3
+c.showPage()
+c.save()
+
+key = input("Wait")
