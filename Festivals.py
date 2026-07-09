6ec33038ad4ec2c4390dcf949dab6c528a8b80ae
jan438 <jan438@users.noreply.github.com>
2026-07-09 09:59:30 +0200

    Update Festivals.py

--- Festivals.py
+++ Festivals.py
@@ -16,9 +16,10 @@ festivalfont = "LiberationSerif"
 festivaldata = []
 templatedata = []
 festivalevents = []
-maxfestivals = 10
-maxfestivalspage = 25
+maxfestivalspage = 9
 index = -1
+bottommargin = 100
+leftmargin = 100
 
 class FestivalEvent:
     def __init__(self, summary, startday, endday, location, description, month):
@@ -170,8 +171,8 @@ def create_Fesival_pdf(filename, ps, pagesize, title="Festivals"):
             c.setFillColor(HexColor('#000000'))
             c.setFont(festivalfont, 12)
             index = lookupfestival(name)
-            festival_x = col * 200
-            festival_y = row * 300
+            festival_x = leftmargin + col * 200
+            festival_y = bottommargin + row * 300
             festival_s = float(festivaldata[index][7])
             octagon(c, x=festival_x, y=festival_y, s=100.0)
             drawing = scaleSVG('SVG/' + name + '.svg', festival_s)
